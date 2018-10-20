049370dd327b068cae277ba7e1b5bdc79bebe906
Lennart Weiss <weislenn@hu-berlin.de>
2018-10-20 18:07:31 +0200

    Use new modules for exercise

--- exercise/print_adjacent_list.c
+++ exercise/print_adjacent_list.c
@@ -1,103 +1,32 @@
 #include "../helpers.h"
-#include <errno.h>
+#include "../adjacency_list.h"
+
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Node {
-  long id;
-  struct Node* next;
-} Node;
-
-typedef struct {
-  Node* nodes;
-  unsigned long size;
-} Graph;
-
-Node* appendEdge(Node* n, unsigned long edge_id) {
-  if(n->id == edge_id)
-    return n;
-  
-  while(n->next != NULL) {
-    if(n->id == edge_id)
-      return n;
-    n = n->next;
-  }
-    
-  n->next = malloc(sizeof(Node));
-  if(n->next == NULL) return NULL;
-
-  n = n->next;
-  n->id = edge_id;
-  n->next = NULL;
-  return n;
-}
-
-void printGraph(Graph g) {
-  for(unsigned long i=0; i<g.size; i++) {
-    Node* n = &g.nodes[i];
-    printf("%lu", i);
-    while(n->next != NULL) {
-      n = n->next;
-      printf("-> %lu", n->id);
-    }
-    printf("\n");
-  }
-}
-
-void destroyNodeList(Node* n) {
-  if(n->next == NULL) return;
-
-  n = n->next; // First pointer was not malloced
-  Node* next;
-  while(n != NULL) {
-    next = n->next;
-    free(n);
-    n = next;
-  }
-}
-
-void destroyGraph(Graph g) {
-  for(int j=0; j<g.size; j++) {
-    destroyNodeList(&g.nodes[j]);
-  }
-  free(g.nodes);
-}
 
 int main() {
-  unsigned long test_cases, edge_count, node_id, edge_id;
-  Graph g;
+  unsigned long test_cases, graph_size, edge_count, node1, node2;
+  Graph* g;
   
   test_cases = get_ul(1, &test_cases);
   for(int i=0; i<test_cases; i++) {
-    if(!get_ul(2, &g.size, &edge_count))
+    if(!get_ul(2, &graph_size, &edge_count))
       return 1;
-    g.nodes = malloc(g.size * sizeof(Node));
-    if(g.nodes == NULL) {
-      fprintf(stderr, "Allocation error\n");
-      return 1;
-    }
+    g = init_graph(graph_size);
     
-    for(int j=0; j<g.size; j++) {
-      g.nodes[j].id = j;
-      g.nodes[j].next = NULL;
-    }
-
     for(int j=0; j<edge_count; j++) {
-      if(!get_ul(2, &node_id, &edge_id)) {
-	destroyGraph(g);
+      if(!get_ul(2, &node1, &node2)) {
+	destroy_graph(g);
 	return 1;
       }
 	
-      if(appendEdge(&g.nodes[node_id], edge_id) == NULL ||
-	 appendEdge(&g.nodes[edge_id], node_id) == NULL) {
-	destroyGraph(g);
+      if(add_edge(g, node1, node2) == NULL ||
+	 add_edge(g, node2, node1) == NULL) {
 	fprintf(stderr, "Allocation error\n");
 	return 1;
       }
     }
-
-    printGraph(g);
-    destroyGraph(g);
+    print_graph(g);
+    destroy_graph(g);
   }
 
   return 0;
